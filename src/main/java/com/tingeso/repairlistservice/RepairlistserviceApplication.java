9ddf1d127ba786e9703601a1ca0a648a52254346
Karen Garrido <karen.garrido.c@usach.cl>
2024-05-31 00:30:17 -0400

    Levantamiento MS

---
+++ src/main/java/com/tingeso/repairlistservice/RepairlistserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.repairlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairlistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairlistserviceApplication.class, args);
+	}
+
+}
